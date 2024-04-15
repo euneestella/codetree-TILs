4dc3c7faf5f2967c433606cf0a141cf8312d6e3a
Eunhee Kim <46220978+euneestella@users.noreply.github.com>
2024-04-15 15:31:03 +0900

    Title: 행복한 수열의 개수, Time: 128ms, Memory: 28MB, Status: Passed - CodeTree

--- 240415/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240415/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,25 +3,39 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-
 cnt = 0
-
-for row in graph:
-    tmp = 1
-    for i in range(len(row)-1):
-        if row[i] == row[i+1]:
-            tmp += 1
-    if tmp >= m:
-        cnt += 1
+flag = False
 
 
-for i in range(n):
-    col = [graph[j][i] for j in range(n)]
-    tmp = 1
-    for k in range(len(col)-1):
-        if col[k] == col[k+1]:
-            tmp += 1
-    if tmp >= m:
-        cnt += 1
+if m == 1:
+    cnt = 2 * n
+else:
+    for row in graph:
+        tmp = 1
+        for i in range(1, len(row)):
+            if row[i] == row[i-1]:
+                tmp += 1
+                if tmp >= m:
+                    flag = True
+            else:
+                tmp = 1
+        if flag:
+            cnt += 1
+        flag = False
+
+    # 각 열을 순회하면서 연속된 같은 값의 개수를 세기
+    for i in range(n):
+        col = [graph[j][i] for j in range(n)]
+        tmp = 1
+        for k in range(1, len(col)):
+            if col[k] == col[k-1]:
+                tmp += 1
+                if tmp >= m:
+                    flag = True
+            else:
+                tmp = 1
+        if flag:
+            cnt += 1
+        flag = False
 
 print(cnt)
