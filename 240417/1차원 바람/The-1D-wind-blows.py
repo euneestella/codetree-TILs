c4e6015979701a5db16c434b0ec66677722c4054
Eunhee Kim <46220978+euneestella@users.noreply.github.com>
2024-04-17 10:38:48 +0900

    Title: 1차원 바람, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240417/1차원 바람/The-1D-wind-blows.py
@@ -0,0 +1,38 @@
+n, m, q = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+winds = [tuple(input().split()) for _ in range(q)]
+
+def blow_wind(r, is_left, grid):
+    if is_left:
+        first = grid[r].pop()
+        grid[r].insert(0, first)
+    else:
+        last = grid[r].pop(0)
+        grid[r].append(last)
+
+
+def process_grid():
+    for wind in winds:
+        r, direction = int(wind[0]) - 1, wind[1]
+        is_left = (direction == 'L')
+        blow_wind(r, is_left, grid)
+
+        for top_r in range(r, 1, -1):
+            for a, b in zip(grid[top_r], grid[top_r+1]):
+                if a == b:
+                    break
+            is_left = not is_left
+            blow_wind(top_r-1, is_left, grid)
+        
+        is_left = (direction == 'L')
+        for bottom_r in range(r+1, n):
+            for a, b in zip(grid[bottom_r], grid[bottom_r-1]):
+                if a == b:
+                    break
+            is_left = not is_left
+            blow_wind(bottom_r, is_left, grid)
+
+process_grid()
+
+for row in grid:
+    print(*row)
